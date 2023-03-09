11348fd624183736ec1b540509fba010946d24c1
Kathange <125526244+Kathange@users.noreply.github.com>
2023-03-09 14:10:38 +0800

    Update main.c

--- Lab#1/main.c
+++ Lab#1/main.c
@@ -4,12 +4,13 @@ int main(){
 	int a[]={1,2,3}, aa[3],i;
 	char b[]={"ABC"};
 	float c[]={1.1,1.2,1.3};
-	FILE *f;
-	f=fopen("a.bin","wb");
-	fwrite(a,sizeof(int),3,f);
-	fread(aa,sizeof(int),3,f);
+	FILE *fp;
+	fp=fopen("a.bin","wb+");
+	fwrite(a,sizeof(int),3,fp);
+	fseek(fp,0,SEEK_SET);
+	fread(aa,sizeof(int),3,fp);
 	for(i=0;i<3;i++)
 		printf("%d ",aa[i]);
-	fclose(f);
+	fclose(fp);
 	return 0;
 } 
